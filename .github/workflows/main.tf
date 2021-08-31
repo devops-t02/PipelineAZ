f03644b682c0bee285544707a1c962fc50aaed7f
Zm-intern02@zmwarelab.com <Zm-intern02@zmwarelab.com>
2021-08-31 04:54:22 -0400

    v1

--- .github/workflows/main.tf
+++ .github/workflows/main.tf
@@ -1,1 +1,48 @@
+terraform {
+
+  required_providers {
+
+    azurerm = {
+
+      source = "hashicorp/azurerm"
+
+      version = ">=2.0"
+
+    }
+  }
+
+  #terraform cloud  
+
+  backend "remote" {
+
+    organization = "zmware"
+
+
+
+
+    workspaces {
+
+      name = "PipelineAZ"
+
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider 
+
+provider "azurerm" {
+  skip_provider_registration = "true"
+  features {}
+}
+
+#create azure container registry  
+
+resource "azurerm_container_registry" "acr" {
+  name                = "myfirstacrazpipeline"
+  resource_group_name = "PipelineAZ"
+  location            = "eastus"
+  sku                 = "Premium"
+  admin_enabled       = false
+
+}
 
